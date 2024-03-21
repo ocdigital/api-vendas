250eed4448fac53627601558b8fc0043b87832a6
Eduardo Ferreira <eduardoferreira85@gmail.com>
2024-03-21 18:03:24 -0300

    Create Seller Model

--- tests/Unit/API/SellerControllerTest.php
+++ tests/Unit/API/SellerControllerTest.php
@@ -1,15 +1,31 @@
 <?php
 
+namespace Tests\Unit\API;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Seller;
+use Tests\TestCase;
+
+uses(TestCase::class);
+
+uses(RefreshDatabase::class);
 
 test('can_create_seller', function () {
     $seller = Seller::factory()->create();
-    $this->assertDatabaseHas('sellers', $seller->toArray());
+
+    $this->assertDatabaseHas('sellers', [
+        'name' => $seller->name,
+        'email' => $seller->email,
+        'id' => $seller->id,
+        'updated_at' => $seller->updated_at,
+        'created_at' => $seller->created_at,
+    ]);
 });
 
 test('can_list_sellers', function () {
-    $sellers = Seller::factory()->count(3)->create();
-    $this->assertCount(3, Seller::all());
+    Seller::factory()->count(3)->create();
+    $count = Seller::count();
+    $this->assertEquals(3, $count);
 });
 
 
