7b815264a4e611fa8842d4d9bfac0cef521114bd
Eduardo Ferreira <eduardoferreira85@gmail.com>
2024-03-21 21:05:15 -0300

    Create seller controller and routes

--- database/seeders/SellerSeeder.php
+++ database/seeders/SellerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Seller;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class SellerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Seller::factory()->count(10)->create();
     }
 }
