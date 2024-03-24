738482e3e3b8dfd4f8ff486635c057ba9752c751
Eduardo Ferreira <eduardoferreira85@gmail.com>
2024-03-24 08:33:47 -0300

    Config algolia and create seller indice

--- backend/app/Models/Seller.php
+++ backend/app/Models/Seller.php
@@ -6,12 +6,15 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
 
 class Seller extends Model
 {
     use HasFactory;
     use HasUuids;
     use SoftDeletes;
+
+    use Searchable;
 
     protected $fillable = ['name', 'email'];
 
---
+++ backend/app/Observers/SellerObserver.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Seller;
+use Laravel\Scout\Searchable;
+
+class SellerObserver
+{
+    use Searchable;
+
+    public function created(Seller $seller)
+    {
+        $seller->searchable();
+    }
+}
